b6cda2f379cef3cb9e4dffa572c6fa003dfcce6e
lyc <lyc@hzlpjywwgc.onexmail.com>
2020-08-06 21:29:19 +0800

    viewmodel

--- app/src/main/java/com/jetpack/kotlin/activity/MainActivity.kt
+++ app/src/main/java/com/jetpack/kotlin/activity/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.jetpack.kotlin.activity
 
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.jetpack.kotlin.base.BaseActivity
 import com.jetpack.kotlin.R
+import com.jetpack.kotlin.vm.MainViewModel
 
 class MainActivity : BaseActivity() {
+    lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override fun initView() {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/jetpack/kotlin/base/BaseActivity.kt
+++ app/src/main/java/com/jetpack/kotlin/base/BaseActivity.kt
@@ -1,15 +1,18 @@
 package com.jetpack.kotlin.base
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
 
 /**
  * @author  lyc
  * @date    2020/8/4
  * @description:
  */
-open class BaseActivity : Activity() {
+open abstract class BaseActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initView();
     }
+
+    abstract fun initView();
 }
